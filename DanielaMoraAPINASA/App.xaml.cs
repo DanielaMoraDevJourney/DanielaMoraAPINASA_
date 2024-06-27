f43a42ff8934d632543ce93ba52eb2e6b6829f3d
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-26 21:16:00 -0500

    Agregar archivos de proyecto.

---
+++ DanielaMoraAPINASA/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DanielaMoraAPINASA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DanielaMoraAPINASA/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DanielaMoraAPINASA
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
